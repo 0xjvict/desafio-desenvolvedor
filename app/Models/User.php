8db1a9d250f1cc85e76ef540b64eb65179a62b7d
jvmelo <jvmelo.dev@gmail.com>
2026-03-27 11:07:39 -0300

    feat(auth): adiciona autenticação com Laravel Sanctum e configurações de API
    Implementa o Laravel Sanctum para autenticação de usuários e adiciona rotas para a API.
    Inclui a criação da tabela de tokens de acesso pessoal e ajustes na classe User para suportar MongoDB.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,15 +7,19 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
+
+    protected $connection = 'mongodb';
+    protected string $collection = 'file_uploads';
 
     /**
      * Get the attributes that should be cast.
