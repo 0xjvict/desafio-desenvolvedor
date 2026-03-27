e7c8dbf6d9c8936bb4bc2e2bff2354b517200b37
jvmelo <jvmelo.dev@gmail.com>
2026-03-27 13:03:52 -0300

    feat(tests): substitui RefreshDatabase por RefreshMongoDatabase
    Atualiza os testes de autenticação para utilizar a nova trait RefreshMongoDatabase, garantindo que o banco de dados MongoDB seja limpo entre os testes.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Tests\Traits\RefreshMongoDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
